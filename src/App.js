2e15aa343b5033aae3d96f2200417ecc3598b37a
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-06-01 16:58:44 -0600

    wireframing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from "./Components/Header"
+import HomePage from "./Components/Homepage/Homepage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+      cart: [],
+      instruments: []
+    }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/for-sale')
+  //     .then(res => res.json())
+  // }
+
+  //addInstrumentsToCart 
+
+  //removeInstrumentsFromCart
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <HomePage />
+        <div className="footer">
+          This is our footer
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
